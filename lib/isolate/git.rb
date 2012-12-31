e8680397fbd123f76426e5806a4a473b07b56dc2
bhenderson <bhenderson@yp.com>
2012-12-31 09:13:26 -0800

    Make it a class so we don't extend any isolate classes.

--- lib/isolate/git.rb
+++ lib/isolate/git.rb
@@ -1,47 +1,119 @@
 require 'isolate'
+require 'fileutils'
 
 module Isolate
-  module Git
+  class Git
 
-    def git?
-      @options.key? :git
+    def self.setup entry
+      new(entry).setup
     end
 
-    def git_build
+    def initialize entry
+      @entry = entry
     end
 
-    def git_clone
-      # silence output?
-      # @sandbox.log s
-      system "git clone #{@options[:git]}"
-    end
+    def build
+      gemspec_path = Dir['**/*.gemspec'].first.to_s
 
-    def git_setup
-      Dir.chdir git_source_path do
-        # clone
-        git_clone
-        # build
-        git_build
-        # set @file in entry
-        @file = git_pkg_path
+      # does the repo contain a gemspec? Just build it.
+      if File.size? gemspec_path
+        sh 'gem', 'build', gemspec_path
+      # try running rake package. Projects that use Hoe for example don't come
+      # with a gemspec, but one can be built. One may have to add build
+      # dependencies to the Isolate file. ie. rake, hoe.
+      elsif File.size? 'Rakefile'
+        sh 'rake', 'package'
+      else
+        # don't know what to do
+      end
+
+      built_path = Dir['**/*.gem'].first.to_s
+
+      if File.size? built_path
+        built_path
+      else
+        log "unable to build or find #{@entry.name} gem"
+        false
       end
     end
 
-    def git_source_path
-      File.join *[
-        @sandbox.path,
-        ('..' if @sandbox.multiruby?),
+    def clone
+      src = @entry.options[:git]
+      name = @entry.name
+
+      if sh 'git','clone', src, name
+        name
+      else
+        log "unable to clone #{src}"
+        false
+      end
+    end
+
+    def git?
+      @entry.options.key? :git
+    end
+
+    # For each entry, if options[:git] exists try to clone the repo and build
+    # the gem. If either fails, warn and let Isolate continue doing it's normal
+    # thing.
+    def setup
+      return unless git?
+
+      Dir.chdir source_path do
+
+        path = clone
+
+        Dir.chdir path do
+
+          if name = build then
+            file = File.expand_path name
+
+            # private instance variable. When this is set Entry#install looks
+            # for a local file instead of trying to download by name
+            @entry.instance_variable_set :@file, file
+          end
+
+        end if path
+
+      end
+    end
+
+    # Where to put the git repos.
+    def source_path
+      src = File.join *[
+        sandbox.path,
+        ('..' if sandbox.multiruby?),
         'src'
       ].compact
+
+      FileUtils.mkdir_p src
+
+      src
+    end
+
+    private
+
+    def log s
+      sandbox.log s
+    end
+
+    def sandbox
+      # I don't like looking in private variables
+      @sandbox ||= @entry.instance_variable_get(:@sandbox)
+    end
+
+    def sh *args
+      IO.popen(args) do |io|
+        log io.gets until io.eof?
+      end
+      $?.success?
     end
 
   end
 
-  class Entry
-    include Git
-  end
-
-  Isolate::Event.watch Isolate::Entry, :installing do |entry|
-    entry.git_setup if entry.git?
+  # setup installing hook so that each entry that has a :git option is setup
+  # correctly for activiation.
+  Events.watch Entry, :installing do |entry|
+    Git.setup entry
   end
 end
